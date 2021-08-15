7aa7574105a35c34858b05d02116ccbd59a70564
MelnykVL <w.vladyslavmelnyk@gmail.com>
2021-08-15 14:13:49 +0300

    Change ConnectionDB.java

--- src/main/java/edu/diploma/dao/ConnectionDB.java
+++ src/main/java/edu/diploma/dao/ConnectionDB.java
@@ -1,31 +1,47 @@
 package edu.diploma.dao;
 
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.util.Properties;
 
 public class ConnectionDB {
 
-    private static final String DB_DRIVER = "org.postgresql.Driver";
-    private static final String DB_URL = "jdbc:postgresql://localhost:5432/ehealth_records";
-    private static final String DB_USERNAME = "postgres";
-    private static final String DB_PASSWORD = "";
+    private static Connection connection = null;
 
     public static Connection getConnection(){
+        if (connection == null) {
+            Properties prop = loadPropertiesFile();
 
-        Connection connection = null;
+            final String DB_DRIVER = prop.getProperty("driver");
+            final String DB_URL = prop.getProperty("url");
+            final String DB_USERNAME = prop.getProperty("username");
+            final String DB_PASSWORD = prop.getProperty("password");
 
-        try{
-            Class.forName(DB_DRIVER);
-            connection = DriverManager.getConnection(DB_URL, DB_USERNAME, DB_PASSWORD);
-            System.out.println("Connection successful");
-        } catch (ClassNotFoundException | SQLException e){
-            e.printStackTrace();
-            System.out.println("Connection failed");
+            try {
+                Class.forName(DB_DRIVER);
+                connection = DriverManager.getConnection(DB_URL, DB_USERNAME, DB_PASSWORD);
+                System.out.println("!====Connection successful====!");
+            } catch (ClassNotFoundException | SQLException e) {
+                e.printStackTrace();
+            }
         }
 
         return connection;
+    }
 
+    private static Properties loadPropertiesFile() {
+        Properties prop = new Properties();
+        try (InputStream in = new FileInputStream("src/main/resources/flyway.properties")) {
+            prop.load(in);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return prop;
     }
 
 }
